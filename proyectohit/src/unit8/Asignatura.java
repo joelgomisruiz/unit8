2818950d8771999e3afe3308c8e4e602ceb31627
joel <joelgomisruiz@gmail.com>
2015-04-29 15:20:48 +0200

    implementacion clase asignatura

--- proyectohit/src/unit8/Asignatura.java
+++ proyectohit/src/unit8/Asignatura.java
@@ -9,11 +9,28 @@ package unit8;
  */
 public class Asignatura {
 
+	private String nombre;
+	private double nota;
 	/**
 	 * 
 	 */
 	public Asignatura() {
 		// TODO Auto-generated constructor stub
+	}
+	public Asignatura(String nombre){
+		this.nombre = nombre;
+	}
+	public String GetNombre(){
+		return nombre;
+	}
+	public void SetNombre(String nombre){
+		this.nombre = nombre;
+	}
+	public String GetDni(){
+		return dni;
+	}
+	public void SetDni(String dni){
+		this.dni = dni;
 	}
 
 }
